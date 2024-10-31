ee879174618f32fe451ffd5af141267657835272
ofiravisar <ofiravisar97@gmail.com>
2024-10-31 22:17:23 +0200

    finished profile header info

--- client/components/Profile/HeaderInfo.vue
+++ client/components/Profile/HeaderInfo.vue
@@ -1,12 +1,17 @@
 <template>
   <header class="py-8 flex gap-4">
     <UIAvatar width="160" height="160" />
-    <div>
+    <div class=" p-2 gap-4 flex flex-col items-center w-full">
       <strong
         ><h1 class="text-xl">
           <span class="text-primary">@</span>Ofir Avisar
         </h1></strong
       >
+      <div class="flex gap-4">
+        <UIProfileLabelCount label="Followers" count="5"/>
+        <UIProfileLabelCount label="Recipes" count="3"/>
+        <UIProfileLabelCount label="Bookmarks" count="10"/>
+      </div>
     </div>
   </header>
 </template>
---
+++ client/components/UI/Profile/LabelCount.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-lg flex flex-col items-center justify-center">
+       <p>{{ label }}</p>
+       <p><strong>{{ count }}</strong></p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const {label,count} = defineProps(['label','count'])
+</script>
