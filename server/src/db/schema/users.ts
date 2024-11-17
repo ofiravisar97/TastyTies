2949e760ad746bf2a0fa07f5825ed95ce51d9b96
Ofir Avisar <ofiravi@std.hac.ac.il>
2024-11-17 02:30:08 +0200

    added some nextjs packages

--- server/src/db/schema/users.ts
+++ server/src/db/schema/users.ts
@@ -1,9 +1,17 @@
-import { sql } from "drizzle-orm";
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
+import { integer, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   email: varchar("email").unique().notNull(),
   password: varchar("password").notNull(),
   avatarURL: varchar("avatar_url").default(sql`NULL`),
+});
+
+export const usersProfile = pgTable("users_profiles", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  followersCount: integer("followers_count").default(0),
+  recipesCount: integer("recipes_count").default(0),
+  bookmarksCount: integer("bookmarks_count").default(0),
+  bio: text("bio").default(sql`NULL`),
 });
